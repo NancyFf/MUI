81886bb0aae9994f5ebd87530655594b1411a307
nancy farias fernandez <isc18350306@gmail.com>
2022-03-17 16:39:14 -0600

    Ejercicio Nan

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,44 @@
-const App = () =>{
+const App = () => {
     const persona = {
-        nombre: "Nancy",
-        apellido: "Farias",
-        edad: "22",
-        direccion: {
-            calle: "Avenida Siempre viva",
-            numExt: 742,
-            numInt: "",
-            ciudad: "Springfield",
-        },
+    nombre: "Nancy",
+    apellido: "Farias",
+    edad: 23,
+    direccion:{
+    calle: "Priv Petra Castro",
+    numExt: 10,
+    numint: "",
+    ciudad: "San Juan Bautista Tuxtpec",
+    }
     };
-
-const contactoPersona = {
+    const contactoPersona = {
     telefono: "5549596991",
     email: "isc18350306@gmail.com",
     web: "www.thesimpsons.com",
-};
-
-const infoPersona ={ ...persona, ...contactoPersona, empresa: "Disney" };
-infoPersona.apodo ="Bart";
-
-infoPersona.telefono ="5555 55 55";
-
-console.table(infoPersona);
-
-const { edad, nombre, apellido } = persona;
-const{ calle, numExt, ciudad } = persona.direccion;
-const humanos = ["Homer", "Margorie", "Lisa", "Maggie"];
-const [padre, madre, hermana1, hermana2] = familia;
-const mascotas = ["Santa's helper", "Snoe ball"];
-
-const familia ["...humanos, ...mascotas, "Milhuse"];
-
-return (
-<div className="App">
-    {/*EcmaScript 6*/}
-    <h1>{`Mi nombrre es ${nombre} ${apellido} y tengo tan solo ${edad} años y vivo en ${calle} numero ${numExt}, ${ciudad}`}</h1>
-
-<br />
-{"Mi familia la conforman:"}
-<ul>
+    };
+    const infoPersona = {...persona,...contactoPersona, empresa: "Disney"};
+    infoPersona.apodo = "Bart";
+    infoPersona.telefono = "555 555 5555";
+    console.table(infoPersona);
+    const{edad, nombre, apellido}= persona;
+    const{calle, numExt, numint,ciudad,} = persona.direccion;
+    const humanos = ["Arturo", "Lulu","Arturo", "Chiquis"];
+    const[padre, madre, hermano1, hermano2]= humanos;
+    const mascotas = ["Santa's helper", "show ball"]
+    const familia =[...humanos,...mascotas, "Milhouse"]
+    console.log(familia) 
+    
+    return (
+    <div className="App">
+    <h1>{`Mi nombre es ${nombre} ${apellido} y tengo ${edad} años y vivo en ${calle} numero ${numExt},${ciudad}`}</h1>
+    <br />
+    {"Mi familia la conforman:"}
+    <ul>
     <li>{`Mi padre: ${padre}`}</li>
     <li>{`Mi madre: ${madre}`}</li>
-    <li>{`Mis hermanas: ${hermana1} y ${hermana2}`}</li>
-</ul>
-</div>
-
-
-);
-};
-
-
-export default App;
+    <li>{`Mis hermanos: ${hermano1} y ${hermano2}`}</li>
+    </ul>
+    </div>
+    );
+    }
+    export default App;
+    
