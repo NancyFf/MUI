4af06c92bfc1164ff5fef04223687cfaeaeb47d7
nancy farias fernandez <isc18350306@gmail.com>
2022-03-17 16:10:02 -0600

    Ejemplo Barth

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-const App = () => {
-    const miObjeto ={
+const App = () =>{
+    const persona = {
         nombre: "Nancy",
-        apPaterno: "Farias",
-        apMaterno: "Fernandez",
+        apellido: "Farias",
         edad: "22",
+        direccion: {
+            calle: "Avenida Siempre viva",
+            numExt: 742,
+            numInt: "",
+            ciudad: "Springfield",
+        },
     };
 
-    const atributo = "apPaterno";
+const contactoPersona = {
+    telefono: "5549596991",
+    email: "isc18350306@gmail.com",
+    web: "www.thesimpsons.com",
+};
+
+const infoPersona ={ ...persona, ...contactoPersona, empresa: "Disney" };
+infoPersona.apodo ="Bart";
+
+infoPersona.telefono ="5555 55 55";
+
+console.table(infoPersona);
+
+const { edad, nombre, apellido } = persona;
+const{ calle, numExt, ciudad } = persona.direccion;
+const humanos = ["Homer", "Margorie", "Lisa", "Maggie"];
+const [padre, madre, hermana1, hermana2] = familia;
+const mascotas = ["Santa's helper", "Snoe ball"];
+
+const familia ["...humanos, ...mascotas, "Milhuse"];
 
 return (
 <div className="App">
-    <h1>{`Mi nombrre es${miObjeto.nombre} ${miObjeto.apPaterno} ${miObjeto.apMaterno} y tengo tan solo ${miObjeto.edad} años`}</h1>
+    {/*EcmaScript 6*/}
+    <h1>{`Mi nombrre es ${nombre} ${apellido} y tengo tan solo ${edad} años y vivo en ${calle} numero ${numExt}, ${ciudad}`}</h1>
+
+<br />
+{"Mi familia la conforman:"}
+<ul>
+    <li>{`Mi padre: ${padre}`}</li>
+    <li>{`Mi madre: ${madre}`}</li>
+    <li>{`Mis hermanas: ${hermana1} y ${hermana2}`}</li>
+</ul>
 </div>
-<h1>
-    {"Mi Nombre es" + miObjeto.nombre + " "+miObjeto.apPaterno+" "+miObjeto.apMaterno+"y tengo"}
-</h1>
+
 
 );
 };
